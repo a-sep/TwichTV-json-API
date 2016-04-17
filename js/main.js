9b696aff759c9f3fbcb99e37c5a866903110cc44
Artur Sep <artur.sep@gmail.com>
2016-04-17 05:18:00 +0200

    added getJSON .fail() for closed accounts

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
 //========= ver. 1.0  ===========
 
-var channels = ["freecodecamp", "storbeck", "terakilobyte", "food", "habathcx", "RobotCaleb", "thomasballinger", "noobs2ninjas", "beohoff", "ESL_SC2", "OgamingSC2", "minecraft", "cretetion"];
+var channels = ["freecodecamp", "brunofin", "storbeck", "terakilobyte", "food", "habathcx", "RobotCaleb", "thomasballinger", "noobs2ninjas", "beohoff", "ESL_SC2", "OgamingSC2", "minecraft", "cretetion"];
 
 function show(name, logo, url, status) {
 
     var html = "";
     html += '<li><div class="row text-center">';
     if (logo) {
-        html += '<div class="col-sm-2"><img src=' + logo + ' class="img-circle"></div>';
+        html += '<div class="col-xs-2"><img src=' + logo + ' class="img-circle"></div>';
 
     } else {
-        html += '<div class="col-sm-2"><i class="fa fa-3x fa-question-circle" aria-hidden="true"></i></div>';
+        html += '<div class="col-xs-2"><i class="fa fa-3x fa-question-circle" aria-hidden="true"></i></div>';
     }
     html += '<div class="col-sm-3"><a href=' + url + ' target="_blank">' + name + '</a></div>';
     html += '<div class="col-sm-7">' + status + '</div>';
@@ -25,7 +25,8 @@ function twichChannels() {
         var name, logo, url, status;
 
         $.getJSON('https://api.twitch.tv/kraken/streams/' + value, function (data) {
-            // console.log(data);
+            console.log(data);
+
             if (data.stream) {
                 name = data.stream.channel.display_name;
                 logo = data.stream.channel.logo;
@@ -35,7 +36,7 @@ function twichChannels() {
             } else {
                 // console.log(value);
                 $.getJSON('https://api.twitch.tv/kraken/channels/' + value, function (data) {
-                    console.log(data.display_name);
+                    // console.log(data.display_name);
                     name = data.display_name;
                     logo = data.logo;
                     url = data.url;
@@ -43,6 +44,11 @@ function twichChannels() {
                     show(name, logo, url, status);
                 });
             }
+        }).fail(function () {
+            name = value;
+            url = "#";
+            status = "Account Closed";
+            show(name, logo, url, status);
         });
     });
 }
